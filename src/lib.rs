37f4ea2b39dd095b74e3ba35aa95a29ba19b6bee
Julian Rosse <julian@helixbass.net>
2023-01-05 16:23:45 -0500

    empty plugin window

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,39 @@
 use nih_plug::{
     buffer::Buffer,
-    context::process::ProcessContext,
+    context::{gui::AsyncExecutor, process::ProcessContext},
+    editor::Editor,
     midi::{MidiConfig, NoteEvent},
     params::Params,
-    plugin::{AuxiliaryBuffers, Plugin, ProcessStatus},
+    plugin::{AuxiliaryBuffers, Plugin, ProcessStatus, Vst3Plugin},
+    prelude::{nih_export_vst3, BoolParam},
 };
+use nih_plug_egui::{create_egui_editor, egui, widgets, EguiState};
+use std::collections::HashSet;
 use std::sync::Arc;
 
+#[derive(Default)]
 struct SustainPlugin {
     params: Arc<SustainPluginParams>,
+    current_notes: HashSet<u8>,
+    sustained_notes: Option<HashSet<u8>>,
 }
 
-impl Default for SustainPlugin {
+#[derive(Params)]
+struct SustainPluginParams {
+    #[persist = "editor-state"]
+    editor_state: Arc<EguiState>,
+
+    #[id = "is-sustaining"]
+    pub is_sustaining: BoolParam,
+}
+
+impl Default for SustainPluginParams {
     fn default() -> Self {
         Self {
-            params: Arc::new(Default::default()),
+            editor_state: EguiState::from_size(300, 180),
+            is_sustaining: BoolParam::new("Is sustaining?", false),
         }
     }
-}
-
-#[derive(Default, Params)]
-struct SustainPluginParams {
-    is_sustaining: bool,
 }
 
 impl Plugin for SustainPlugin {
@@ -32,8 +44,8 @@ impl Plugin for SustainPlugin {
 
     const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-    const DEFAULT_INPUT_CHANNELS: u32 = 0;
-    const DEFAULT_OUTPUT_CHANNELS: u32 = 0;
+    // const DEFAULT_INPUT_CHANNELS: u32 = 0;
+    // const DEFAULT_OUTPUT_CHANNELS: u32 = 0;
 
     const MIDI_INPUT: MidiConfig = MidiConfig::MidiCCs;
     const MIDI_OUTPUT: MidiConfig = MidiConfig::MidiCCs;
@@ -53,11 +65,65 @@ impl Plugin for SustainPlugin {
     ) -> ProcessStatus {
         while let Some(event) = context.next_event() {
             match event {
-                NoteEvent::NoteOff { .. } => (),
+                NoteEvent::NoteOn {
+                    timing,
+                    voice_id,
+                    channel,
+                    note,
+                    velocity,
+                } => {
+                    self.current_notes.insert(note);
+                    context.send_event(NoteEvent::NoteOn {
+                        timing,
+                        voice_id,
+                        channel,
+                        note,
+                        velocity,
+                    });
+                }
+                NoteEvent::NoteOff {
+                    timing,
+                    voice_id,
+                    channel,
+                    note,
+                    velocity,
+                } => {
+                    if !matches!(
+                        self.sustained_notes.as_ref(),
+                        Some(sustained_notes) if sustained_notes.contains(&note)
+                    ) {
+                        self.current_notes.remove(&note);
+                        context.send_event(NoteEvent::NoteOff {
+                            timing,
+                            voice_id,
+                            channel,
+                            note,
+                            velocity,
+                        });
+                    }
+                }
                 event => context.send_event(event),
             }
         }
 
         ProcessStatus::Normal
     }
+
+    fn editor(&self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
+        create_egui_editor(
+            self.params.editor_state.clone(),
+            (),
+            |_, _| {},
+            move |egui_ctx, setter, _state| {
+                egui::CentralPanel::default().show(egui_ctx, |ui| {});
+            },
+        )
+    }
 }
+
+impl Vst3Plugin for SustainPlugin {
+    const VST3_CLASS_ID: [u8; 16] = *b"SustainPluginzzz";
+    const VST3_CATEGORIES: &'static str = "Instrument|Tools";
+}
+
+nih_export_vst3!(SustainPlugin);
