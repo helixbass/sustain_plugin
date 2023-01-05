34206c56c66012b4f5e784b9902f1226aba1f057
Julian Rosse <julian@helixbass.net>
2023-01-05 17:49:57 -0500

    working

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,17 @@ use nih_plug::{
     prelude::{nih_export_vst3, BoolParam},
 };
 use nih_plug_egui::{create_egui_editor, egui, widgets, EguiState};
-use std::collections::HashSet;
-use std::sync::Arc;
+use std::collections::HashMap;
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
 
 #[derive(Default)]
 struct SustainPlugin {
     params: Arc<SustainPluginParams>,
-    current_notes: HashSet<u8>,
-    sustained_notes: Option<HashSet<u8>>,
+    current_notes: HashMap<u8, NoteEvent>,
+    sustained_notes: Option<HashMap<u8, NoteEvent>>,
 }
 
 #[derive(Params)]
@@ -23,15 +26,17 @@ struct SustainPluginParams {
     #[persist = "editor-state"]
     editor_state: Arc<EguiState>,
 
-    #[id = "is-sustaining"]
-    pub is_sustaining: BoolParam,
+    // #[id = "is-sustaining"]
+    // pub is_sustaining: BoolParam,
+    is_sustaining: AtomicBool,
 }
 
 impl Default for SustainPluginParams {
     fn default() -> Self {
         Self {
             editor_state: EguiState::from_size(300, 180),
-            is_sustaining: BoolParam::new("Is sustaining?", false),
+            // is_sustaining: BoolParam::new("Is sustaining?", false),
+            is_sustaining: Default::default(),
         }
     }
 }
@@ -63,36 +68,21 @@ impl Plugin for SustainPlugin {
         _aux: &mut AuxiliaryBuffers,
         context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
-        while let Some(event) = context.next_event() {
-            match event {
-                NoteEvent::NoteOn {
-                    timing,
-                    voice_id,
-                    channel,
-                    note,
-                    velocity,
-                } => {
-                    self.current_notes.insert(note);
-                    context.send_event(NoteEvent::NoteOn {
+        if self.params.is_sustaining.load(Ordering::SeqCst) && self.sustained_notes.is_none() {
+            self.sustained_notes = Some(self.current_notes.clone());
+        } else if !self.params.is_sustaining.load(Ordering::SeqCst)
+            && self.sustained_notes.is_some()
+        {
+            let sustained_notes = self.sustained_notes.take().unwrap();
+            for (sustained_note, sustained_note_event) in sustained_notes {
+                match sustained_note_event {
+                    NoteEvent::NoteOn {
                         timing,
                         voice_id,
                         channel,
                         note,
                         velocity,
-                    });
-                }
-                NoteEvent::NoteOff {
-                    timing,
-                    voice_id,
-                    channel,
-                    note,
-                    velocity,
-                } => {
-                    if !matches!(
-                        self.sustained_notes.as_ref(),
-                        Some(sustained_notes) if sustained_notes.contains(&note)
-                    ) {
-                        self.current_notes.remove(&note);
+                    } => {
                         context.send_event(NoteEvent::NoteOff {
                             timing,
                             voice_id,
@@ -101,8 +91,32 @@ impl Plugin for SustainPlugin {
                             velocity,
                         });
                     }
+                    _ => unreachable!(),
                 }
-                event => context.send_event(event),
+                self.current_notes.remove(&sustained_note);
+            }
+        }
+
+        while let Some(event) = context.next_event() {
+            let mut dont_forward_event = false;
+            match &event {
+                NoteEvent::NoteOn { note, .. } => {
+                    self.current_notes.insert(*note, event.clone());
+                }
+                NoteEvent::NoteOff { note, .. } => {
+                    if matches!(
+                        self.sustained_notes.as_ref(),
+                        Some(sustained_notes) if sustained_notes.contains_key(note)
+                    ) {
+                        dont_forward_event = true;
+                    } else {
+                        self.current_notes.remove(note);
+                    }
+                }
+                _ => (),
+            }
+            if !dont_forward_event {
+                context.send_event(event);
             }
         }
 
@@ -110,14 +124,24 @@ impl Plugin for SustainPlugin {
     }
 
     fn editor(&self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
-        create_egui_editor(
-            self.params.editor_state.clone(),
-            (),
-            |_, _| {},
+        create_egui_editor(self.params.editor_state.clone(), (), |_, _| {}, {
+            let params = self.params.clone();
             move |egui_ctx, setter, _state| {
-                egui::CentralPanel::default().show(egui_ctx, |ui| {});
-            },
-        )
+                egui::CentralPanel::default().show(egui_ctx, |ui| {
+                    if !params.is_sustaining.load(Ordering::SeqCst)
+                    /*.value()*/
+                    {
+                        if ui.add(egui::Button::new("Sustain")).clicked() {
+                            params.is_sustaining.store(true, Ordering::SeqCst);
+                        }
+                    } else {
+                        if ui.add(egui::Button::new("Stop sustaining")).clicked() {
+                            params.is_sustaining.store(false, Ordering::SeqCst);
+                        }
+                    }
+                });
+            }
+        })
     }
 }
 
